9c058023cca3467b1f3b443e571318f234cc5418
khaled-codeur <manou-codeur@outlook.com>
2020-06-15 18:37:12 +0100

    there is an issue

--- react-app-2/src/App.js
+++ react-app-2/src/App.js
@@ -15,21 +15,47 @@ class App extends Component {
     numberOfEls: 0
   }
 
-  handleIncrement = (e) => {
+  handleIncrement = e => {
     let _id = e.target.parentNode.id;
+    if (this.state.elsArr[_id - 1].val === 0) {
+      e.target.nextElementSibling.className = "controls gray";
+      this.setState({numberOfEls: this.state.numberOfEls + 1})
+    }
     let elsArrCopy = [...this.state.elsArr];
-    elsArrCopy[_id - 1].val += 1;
+    let index = _id - 1;
+    elsArrCopy[index].val += 1;
     this.setState({elsArr: elsArrCopy});
   }
 
-  handleDelete = (e) => {
+  handleDelete = e => {
     let _id = e.target.parentNode.id;
     let elsArrCopy = [...this.state.elsArr];
-    elsArrCopy.splice((_id - 1), 1);
-    this.setState({elsArr: elsArrCopy});
+    let index = elsArrCopy.findIndex(el => el.id === parseInt(_id));
+    elsArrCopy.splice(index, 1)
+    this.setState({elsArr: elsArrCopy, numberOfEls: this.state.numberOfEls - 1});
   }
 
-  
+  handleDecrement = e => {
+    let _id = e.target.parentNode.id;
+    if (this.state.elsArr[_id - 1].val === 1) {
+      e.target.className = "controls lithGray";
+      this.setState({numberOfEls: this.state.numberOfEls - 1})
+    }
+    let index = _id - 1;
+    if (this.state.elsArr[index].val > 0) {
+      let elsArrCopy = [...this.state.elsArr];
+      elsArrCopy[index].val -= 1;
+      this.setState({elsArr: elsArrCopy});
+    }
+  }
+
+  handleReset = () => {
+    let resetedData = [...this.state.elsArr];
+    for (let els of resetedData) {
+      els.val = 0;
+    }
+    this.setState({elsArr: resetedData, numberOfEls: 0})
+  }
 
   render() { 
     return ( 
@@ -40,6 +66,7 @@ class App extends Component {
          deleteItem={this.handleDelete}
          increment={this.handleIncrement}
          decrement={this.handleDecrement}
+         resetVal={this.handleReset}
          />
       </React.Fragment>
      );
--- react-app-2/src/components/Item.jsx
+++ react-app-2/src/components/Item.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 class Item extends Component {
 
+    updateValue = () => {
+        if (this.props.val === 0) return "Zero";
+        else return this.props.val;
+    }
+
     render() { 
         return ( 
             <div className="item" id={this.props._id}>
-                <span className="quantitie yellow">{this.props.val}</span>
+                <span className="quantitie yellow">{this.updateValue()}</span>
                 <button onClick={this.props.OnIncrement} className="controls gray">+</button>
                 <button onClick={this.props.OnDecrement} className="controls lithGray">-</button>
                 <button onClick={this.props.OnDelete} className="controls red">X</button>
