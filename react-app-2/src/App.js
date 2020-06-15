666c0614a391664c16afb9fc53a79993c232a941
khaled-codeur <manou-codeur@outlook.com>
2020-06-15 19:05:41 +0100

    start the test

--- react-app-2/src/App.js
+++ react-app-2/src/App.js
@@ -16,45 +16,19 @@ class App extends Component {
   }
 
   handleIncrement = e => {
-    let _id = e.target.parentNode.id;
-    if (this.state.elsArr[_id - 1].val === 0) {
-      e.target.nextElementSibling.className = "controls gray";
-      this.setState({numberOfEls: this.state.numberOfEls + 1})
-    }
-    let elsArrCopy = [...this.state.elsArr];
-    let index = _id - 1;
-    elsArrCopy[index].val += 1;
-    this.setState({elsArr: elsArrCopy});
+    
   }
 
   handleDelete = e => {
-    let _id = e.target.parentNode.id;
-    let elsArrCopy = [...this.state.elsArr];
-    let index = elsArrCopy.findIndex(el => el.id === parseInt(_id));
-    elsArrCopy.splice(index, 1)
-    this.setState({elsArr: elsArrCopy, numberOfEls: this.state.numberOfEls - 1});
+    
   }
 
   handleDecrement = e => {
-    let _id = e.target.parentNode.id;
-    if (this.state.elsArr[_id - 1].val === 1) {
-      e.target.className = "controls lithGray";
-      this.setState({numberOfEls: this.state.numberOfEls - 1})
-    }
-    let index = _id - 1;
-    if (this.state.elsArr[index].val > 0) {
-      let elsArrCopy = [...this.state.elsArr];
-      elsArrCopy[index].val -= 1;
-      this.setState({elsArr: elsArrCopy});
-    }
+   
   }
 
   handleReset = () => {
-    let resetedData = [...this.state.elsArr];
-    for (let els of resetedData) {
-      els.val = 0;
-    }
-    this.setState({elsArr: resetedData, numberOfEls: 0})
+   
   }
 
   render() { 
